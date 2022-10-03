e48febf60f30fcb5cbe77ce04c7cb2de865140d4
redjay48 <rajdey4892@gmail.com>
2022-10-03 17:19:57 +0530

    Add module pattern for project

--- src/index.js
+++ src/index.js
@@ -1,72 +1,126 @@
-const projectBtn = document.getElementById('new-project');
-const displayProject = document.getElementById('display-project');
+const newProjectBtn = document.getElementById('new-project');
+const displayProjectSelection = document.getElementById('display-project');
 const saveProjectBtn = document.getElementById('save-project');
 const setDefaultBtn = document.getElementById('set-default');
+const deleteProjectBtn = document.getElementById('delete-project');
 
 
 
-// Add new Project div to DOM while hiding the previous Project div
-projectBtn.addEventListener('click', addProject);
 
-let count = 0;
-function addProject() {
-    count++;
-    const Array = [];
-    Array.push(count);
-    for (let i = 0; i < Array.length; i++) {
-        
-        //Call function to Populate project div with task button and table with a header
-        projectDetails(Array[count], count);
+
+const project = (() => {
+    const projectArray = [];
+
+    const createProjectDiv = () => {
+        newdiv = document.createElement('div');
+        const taskDiv = document.createElement('div');
+        const taskBtn = document.createElement('button');
+        const projectName = document.createElement('p');
+        taskDiv.classList.add('task-div');
+        projectName.classList.add('project-name');
+        taskBtn.classList.add('task-btn');
+        taskBtn.textContent = "Add New Task";
+        taskDiv.appendChild(projectName);
+        taskDiv.appendChild(taskBtn);
+        newdiv.appendChild(taskDiv);
+        document.body.appendChild(newdiv);
+        newdiv.classList.add('project');
+        table.createToDoHead(newdiv);
+        hidePreviousProjectDiv();
+
+        taskBtn.addEventListener('click', table.addNewTaskToTable);
 
     }
-    if (count > 1) {
-        //Hide Previous Project div
-        projectHide(Array[count - 1]);
+
+    const pushProjectDivToArray = (name) => {
+        const current = document.body.children[3]
+        projectArray.push(current);
+        current.classList.add(`${name}`);
+        console.log(projectArray);
     }
 
-}
 
-function projectDetails(newdiv, num) {
-
-    newdiv = document.createElement('div');
-    const taskDiv = document.createElement('div');
-    const taskBtn = document.createElement('button');
-    taskDiv.classList.add('task-div');
-    taskBtn.classList.add('task-btn');
-    taskBtn.textContent = "Add New Task";
-    taskDiv.appendChild(taskBtn);
-    newdiv.appendChild(taskDiv);
-    document.body.appendChild(newdiv);
-    newdiv.classList.add('project');
-    newdiv.classList.add(`project-${num}`);
-
-    //function to save current div into an array
-    saveProject(newdiv);
-
-    //function to create a table with a header and header titles
-    createToDoHead(newdiv);
-
-    //button to call function to add new row to todo list
-    taskBtn.addEventListener('click', addNewTask);
-}
-
-function addNewTask() {
-    const tblBody = document.body.children[3].children[1].children[0];
-    const taskRow = document.createElement('tr');
-    for (let i = 0; i < 8; i++) {
-        const cell = document.createElement('td');
-        cell.classList.add('cell');
-        cell.textContent = `data ${i}`;
-        taskRow.appendChild(cell);
+    const hidePreviousProjectDiv = () => {
+        if (document.body.children.length > 4) {
+            const previousDiv = document.body.children[3];
+            document.body.removeChild(previousDiv);
+        }
     }
-    tblBody.appendChild(taskRow);
-}
 
-const projectArray = [];
-function saveProject(currentdiv) {
-    projectArray.push(currentdiv);
-    console.log(projectArray);
-}
+
+    const saveProjectToSelection = () => {
+        const currentDiv = document.body.children[3];
+        if (currentDiv.classList.length === 2) {
+            alert('Project Already Saved!!');
+            return;
+        }
+        const option = document.createElement('option');
+        const projectName = prompt("Please Enter Project Name");
+        option.text = projectName;
+        option.value = projectName.toLowerCase().split(/[ ,]+/).join('-');
+        className = option.value;
+        displayProjectSelection.add(option);
+        // Project Name <p>
+        currentDiv.children[0].children[0].textContent = option.text.toUpperCase();;
+        pushProjectDivToArray(className);
+    }
+
+
+    const setDefaultProject = () => {
+        const currentDiv = document.body.children[3];
+        if (currentDiv.classList.length !== 2) {
+            alert('Please Save Project First!!!');
+        } else {
+            const DEFAULT = currentDiv;
+            console.log(DEFAULT);
+            console.log(DEFAULT.classList);
+        }
+
+    }
+
+
+    const deleteProject = () => {
+        const currentDiv = document.body.children[3];
+        if (currentDiv.classList.length === 2) {
+            if (confirm("Are You Sure You Want To Delete This Saved Project?")) {
+                document.body.removeChild(currentDiv);
+            }
+        } else if (currentDiv.children[1].children[0].children.length > 1) {
+            if (confirm("There is unsaved Data in this project. Do you still want to delete this Project?")) {
+                document.body.removeChild(currentDiv);
+            }
+        } else {
+            document.body.removeChild(currentDiv);
+        }
+    }
+
+
+    const displayProject = (e) => {
+        const currentDiv = document.body.children[3];
+        for (let i = 0; i < projectArray.length; i++) {
+            const classListArray = Array.from(projectArray[i].classList);
+            if (classListArray.includes(e.target.value)) {
+                document.body.removeChild(currentDiv);
+                document.body.appendChild(projectArray[i]);
+            }
+        }
+
+        console.log(e.target.value);
+    }
+    return {
+        projectArray,
+        createProjectDiv,
+        pushProjectDivToArray,
+        saveProjectToSelection,
+        setDefaultProject,
+        deleteProject,
+        displayProject
+    }
+})();
+
+
+
+
 
 
 // Button to call function to save project div as option into the display project select input
